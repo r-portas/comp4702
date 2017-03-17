6624501ea99c7aedf57fc1706d618e873ab06b96
Roy Portas <royportas@gmail.com>
2017-03-17 13:59:47 +1000

    Worked on q1

--- pracs/week3/q1.m
+++ pracs/week3/q1.m
@@ -10,17 +10,42 @@ num = 20;
 err = randn(num, 1);
 upper = 5;
 lower = 0;
-rand_x = (upper - lower) .* rand(num, 1) + lower;
-rand_y = 2 * sin(1.5 * rand_x);
-rand_y = rand_y + err;
 
+% Testing data
+test_x = (upper - lower) .* rand(num, 1) + lower;
+test_y = 2 * sin(1.5 * test_x);
+test_y = test_y + err;
+
+% Validation data
+validation_x = (upper - lower) .* rand(num, 1) + lower;
+validation_y = 2 * sin(1.5 * validation_x);
+validation_y = validation_y + err;
 
 % Create the first plot
-subplot(2, 2, 1);
+subplot(4, 4, 1);
 
 hold on;
 plot(x, y);
-scatter(rand_x, rand_y);
+scatter(test_x, test_y);
 axis([0, 5, -5, 5]);
-title('(a) Function and data');
+title('Function and data');
 hold off;
+
+% Do the lines of best fit
+for degree = 1:10
+   subplot(4, 4, degree + 1);
+   
+   hold on;
+   % S contains the error
+   p = polyfit(test_x, test_y, degree);
+   y = polyval(p, x, S);
+   plot(x, y);
+   
+   r = (validation_y - y) .^ 2;
+   SSE = sum(r)
+   
+   scatter(validation_x, validation_y);
+   axis([0, 5, -5, 5]);
+   title(sprintf('Degree %d line fit', degree));
+   hold off;
+end
