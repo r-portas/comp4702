b8243c237f9a403ee38805e91f4f182c5cf23dfc
Roy Portas <royportas@gmail.com>
2017-03-22 07:00:39 +1000

    Smoothed line in q4

--- pracs/week3/q4.m
+++ pracs/week3/q4.m
@@ -17,42 +17,40 @@ function q4(data, class)
     % scatter(1:length(class1), class1);
     % scatter(1:length(class2), class2);
 
+    estimate_range = 1:0.1:8;
+
     class1_data = data;
     class1_data(class2 == 1) = NaN;
 
     class1_mle = mle(class1_data);
-    class1_pdf = normpdf(x_values, class1_mle(1), class1_mle(2));
+    class1_pdf = normpdf(estimate_range, class1_mle(1), class1_mle(2));
 
     class2_data = data;
     class2_data(class1 == 1) = NaN;
 
     class2_mle = mle(class2_data);
-    class2_pdf = normpdf(x_values, class2_mle(1), class2_mle(2));
+    class2_pdf = normpdf(estimate_range, class2_mle(1), class2_mle(2));
 
     % Verify the classes are divided
     figure;
 
-    subplot(1, 2, 1);
     hold on;
-    scatter(x_values, class1_data);
-    scatter(x_values, class2_data);
-    legend('Iris Setosa', 'Iris Versicolor');
-    ylim([0, 10]);
-    hold off;
 
-    subplot(1, 2, 2);
-    hold on;
-    plot(class1_pdf, x_values);
-    plot(class2_pdf, x_values);
-    ylim([0, 10]);
-    hold off;
+    yyaxis left;
+    scatter(class1_data, x_values, 'r');
+    scatter(class2_data, x_values, 'b');
+
+    yyaxis right;
+    plot(estimate_range, class1_pdf, 'r');
+    plot(estimate_range, class2_pdf, 'b');
+    legend('Iris Setosa', 'Iris Versicolor');
     hold off;
 
     % Plot the likelihood
     figure;
     hold on;
-    plot(x_values, class1_pdf);
-    plot(x_values, class2_pdf);
+    plot(estimate_range, class1_pdf);
+    plot(estimate_range, class2_pdf);
     xlim([1, 10]);
 
     title('Likelihoods');
@@ -61,5 +59,24 @@ function q4(data, class)
     
     hold off;
 
+    % Get the posteriors
+    post_class1 = zeros(71, 1);
+    post_class2 = zeros(71, 1);
+
+    for i = 1:71
+        if class1_pdf(i) < class2_pdf(i) 
+            % Class 2 is bigger
+            post_class2(i) = 1;
+        else 
+            % Class 1 is bigger
+            post_class1(i) = 1;
+        end
+    end
+    figure;
+
+    hold on;
+    plot(1:71, post_class1);
+    plot(1:71, post_class1);
+    hold off;
     
 end
