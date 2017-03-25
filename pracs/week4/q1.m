95da07a5cd7a09dc01cf233a1a16d47bf02ec978
Roy Portas <royportas@gmail.com>
2017-03-26 09:22:47 +1000

    Worked on first assignment

--- pracs/week4/q1.m
+++ pracs/week4/q1.m
@@ -8,7 +8,8 @@ raw = readtable('pima_indians_diabetes.csv');
 raw_data = raw(:, 1:8);
 
 % The classfied classes, e.g. positive or negative
-actual = raw(:, 9);
+actual = table2array(raw(:, 9));
+training_actual = actual(1:500);
 
 % Convert the table into an array
 data = table2array(raw_data);
@@ -17,10 +18,30 @@ data = table2array(raw_data);
 training = data(1:500, :);
 testing = data(500:end, :);
 
-% Create the quadratic classifier
+% Divide training data into positive and negative samples
+
+%% An array of data
+training_positive = NaN(500, 8);
+training_negative = NaN(500, 8);
+
+%% Binary array of samples
+positive = zeros(1, 500);
+negative = zeros(1, 500);
+for i = 1:500
+    if strcmp(training_actual(i), 'pos')
+        positive(i) = 1;
+        training_positive(i, :) = training(i, :);
+    else
+        negative(i) = 1;
+        training_negative(i, :) = training(i, :);
+    end
+end
 
 % Find the covariance of the data
 covariance = cov(training);
+
+% Find the correlation
+correlation = corr(training);
 
 % Find the mean vector (mu)
 mean_vector = zeros(1, 8);
@@ -29,14 +50,32 @@ for i = 1:8
     mean_vector(i) = mean(training(:, i));
 end
 
-mean_vector = transpose(mean_vector);
+% Equation 5.22
+m1 = nanmean(training_positive);
+m2 = nanmean(training_negative);
 
+S1 = nancov(training_positive);
+S2 = nancov(training_negative);
 
-% Use the quadratic discriminant function (Eq. 5.9)
-p1 = 1 / ((2*pi)^(8/2) * sqrt(det(covariance)));
-p2 = exp(-1/2 * transpose(training - mean_vector) * inv(covariance) * (training - mean_vector));
+p1 = sum(positive)/length(positive);
+p2 = sum(negative)/length(negative);
 
-p = p1 * p2;
+res = qd(testing, S1, m1, p1);
 
 % Using mvnpdf
 pdf = mvnpdf(training, mean_vector, covariance);
+
+% Use built in matlab functions
+quadratic = fitcdiscr(training, training_actual, 'DiscrimType', 'quadratic');
+linear = fitcdiscr(training, training_actual);
+
+predict(quadratic, testing);
+predict(linear, testing);
+
+function g = qd(x, S, m, p)
+    % Equation 5.19
+    % g = -1/2 * log(det(S)) - 1/2 * (x - m) * inv(S) * transpose(x - m) + log(p);
+
+    % Equation 5.22
+    g = -1/2 * transpose(x - m) * inv(S) * (x - m) + log(p);
+end
--- pracs/week4/q5.m
+++ pracs/week4/q5.m
@@ -2,7 +2,16 @@
 
 x = [randn(30, 1); 5 + randn(30, 1)];
 
+subplot(2, 2, 1);
+hold on;
+hist(x, 20);
+title('Histogram with 20 bins');
+xlabel('X values');
+ylabel('Probability');
+hold off;
+
 % Demo the results
+subplot(2, 2, 2);
 yyaxis left;
 hist(x, 20);
 yyaxis right;
@@ -16,18 +25,38 @@ yyaxis right;
 [K2, k2x, bw2] = ksdensity(x, 'width', bw1/2);
 
 hold on;
-
 plot(k1x, K1, 'r');
 plot(k2x, K2, 'g');
 
-% Generate 20 random datapoints between that covers the range of data in x 
-x_points = min(x):((max(x)-min(x))/19):max(x);
-scatter(x_points, zeros(1, 20));
+title('K1 and K2 kernel densities');
+legend('Data', 'K1', 'K2');
+hold off;
+
+% Generate 100 random datapoints between that covers the range of data in x 
+x_points = min(x):((max(x)-min(x))/99):max(x);
 
 % Calculate the Guassian mixed model
 mixed = K1/2 + K2/2;
+
+subplot(2, 2, 3);
+hold on;
+yyaxis left;
+hist(x, 20);
+yyaxis right;
 plot (k1x, mixed, 'b');
 
+title('Mixed Model');
+legend('Data', 'Mixed model');
+hold off;
+
+
+subplot(2, 2, 4);
+hold on;
+hist(x, 20);
+scatter(x_points, zeros(1, 100), 'r');
+
+title('Test points');
+legend('Data', 'Test points');
 hold off;
 
 % Do the calculations
@@ -35,12 +64,12 @@ KL(mixed, x, x_points)
 KL(mixed, K1, x_points)
 KL(mixed, K2, x_points)
 
-
 %
 % Calculate the KL Divergence
 %
 function result = KL(p, q, x)
-    px = normpdf(x, mean(p), std(p)^2);
-    qx = normpdf(x, mean(q), std(q)^2);
+    % Convert both to pdfs
+    px = normpdf(x, mean(p), std(p));
+    qx = normpdf(x, mean(q), std(q));
     result = sum(px * log(px/qx));
 end
