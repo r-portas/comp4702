9ff4e2133e71d2a51535f2918feaf413ee5390a5
Roy Portas <royportas@gmail.com>
2017-03-27 07:57:39 +1000

    Worked on q5

--- pracs/week4/q5.m
+++ pracs/week4/q5.m
@@ -16,13 +16,32 @@ yyaxis left;
 hist(x, 20);
 yyaxis right;
 
+% Generate 100 random datapoints between that covers the range of data in x 
+x_points = min(x):((max(x)-min(x))/99):max(x);
+
+% Generate the histogram bin counts
+[dist, edges] = histcounts(x, 20);
+N = sum(dist);
+x0 = edges(1);
+h = edges(2) - edges(1);
+
+data = zeros(1, 100);
+
+for i = 1:100
+    bin = ceil((x_points(i) - x0) / h);
+    x_in_bin = dist(bin);
+    
+    data(i) = x_in_bin / (N * h);
+end
+
+
 % Create the kernel density estimators
 
 % Default kernel width
-[K1, k1x, bw1] = ksdensity(x);
+[K1, k1x, bw1] = ksdensity(x, x_points);
 
 % Half of the default kernel width
-[K2, k2x, bw2] = ksdensity(x, 'width', bw1/2);
+[K2, k2x, bw2] = ksdensity(x, x_points, 'width', bw1/2);
 
 hold on;
 plot(k1x, K1, 'r');
@@ -31,9 +50,6 @@ plot(k2x, K2, 'g');
 title('K1 and K2 kernel densities');
 legend('Data', 'K1', 'K2');
 hold off;
-
-% Generate 100 random datapoints between that covers the range of data in x 
-x_points = min(x):((max(x)-min(x))/99):max(x);
 
 % Calculate the Guassian mixed model
 mixed = K1/2 + K2/2;
@@ -60,9 +76,9 @@ legend('Data', 'Test points');
 hold off;
 
 % Do the calculations
-KL(mixed, x, x_points)
-KL(mixed, K1, x_points)
-KL(mixed, K2, x_points)
+KL(mixed, x, x_points);
+KL(mixed, K1, x_points);
+KL(mixed, K2, x_points);
 
 %
 % Calculate the KL Divergence
